832494406b2321a9cfa85e96ef453c5394d7d740
Guteu <danielguerra1711@gmail.com>
2023-11-27 15:19:41 -0300

    Aula 13 e 14

---
+++ Aula13/ambiente.js
@@ -0,0 +1,19 @@
+var c = 1
+
+do {
+    console.log(c)
+    c++
+} while (c <= 5)
+
+/*
+while (c <= 5) {
+    console.log(c)
+    c++  //c++ == c + 1
+}
+*/
+
+//console.log('tudo bem?')
+//console.log('tudo bem?')
+//console.log('tudo bem?')
+//console.log('tudo bem?')
+//console.log('tudo bem?')
